9f28996360ec875855ca4d8110f261abc6d661ab
JunyuYan <63087360+JunyuYan@users.noreply.github.com>
2020-04-23 18:44:53 +0800

    Add files via upload

---
+++ translations/translation_en_US.ts
@@ -0,0 +1,50 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior.xar:/Animated Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior.xar:/Get Localized Text</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>865/5000
+Here we are now in the study. On the walls you see so-called Nao Marks. I'm also involved in another project, where as a Woter Baddy I make sure that mostly older people take enough liquid. In case of doubt I bring water to them too. These marks allow me with simple means to orient myself in space and to determine my position. This is especially useful for me at the beginning of developing programs. On my left you can see the idea Tatsch Teibl, which can be used to work. In addition, a different Tiem works with it to represent pseudo 3D holograms. Behind the walls is the entire technology for the control of all devices here in the room. Now it's on to the next room. Please make room and let me through.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">865/5000
+Here we are now in the study. On the walls you see so-called Nao Marks. I'm also involved in another project, where as a Woter Baddy I make sure that mostly older people take enough liquid. In case of doubt I bring water to them too. These marks allow me with simple means to orient myself in space and to determine my position. This is especially useful for me at the beginning of developing programs. On my left you can see the idea Tatsch Teibl, which can be used to work. In addition, a different Tiem works with it to represent pseudo 3D holograms. Behind the walls is the entire technology for the control of all devices here in the room. Now it's on to the next room. Please make room and let me through.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>865/5000
+Here we are now in the study. On the walls you see so-called Nao Marks. I'm also involved in another project, where as a Woter Baddy I make sure that mostly older people take enough liquid. In case of doubt I bring water to them too. These marks allow me with simple means to orient myself in space and to determine my position. This is especially useful for me at the beginning of developing programs. On my left you can see the idea Tatsch Teibl, which can be used to work. In addition, a different Tiem works with it to represent pseudo 3D holograms. Behind the walls is the entire technology for the control of all devices here in the room. Now it's on to the next room. Please make room and let me through.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">865/5000
+Here we are now in the study. On the walls you see so-called Nao Marks. I'm also involved in another project, where as a Woter Baddy I make sure that mostly older people take enough liquid. In case of doubt I bring water to them too. These marks allow me with simple means to orient myself in space and to determine my position. This is especially useful for me at the beginning of developing programs. On my left you can see the idea Tatsch Teibl, which can be used to work. In addition, a different Tiem works with it to represent pseudo 3D holograms. Behind the walls is the entire technology for the control of all devices here in the room. Now it's on to the next room. Please make room and let me through.</translation>
+        </message>
+    </context>
+</TS>
